3f61d6ff048ee532aef1d07e787189c05a6a17b5
Shan <shan_ng2000@outlook.com>
2019-10-07 10:05:35 +0800

    Files

---
+++ sifs-files/library/mkdir.c
@@ -0,0 +1,8 @@
+#include "sifs-internal.h"
+
+// make a new directory within an existing volume
+int SIFS_mkdir(const char *volumename, const char *pathname)
+{
+    SIFS_errno	= SIFS_ENOTYET;
+    return 1;
+}
