420d02a119a302c45aad908da3c67b9098c5cbcd
frinzekt <frinzekt@gmail.com>
2019-10-09 01:53:53 +0800

    getDirBlockIDByID Finished
    this will be used for getDirBlockByName by matching

--- sifs-files/library/helper.c
+++ sifs-files/library/helper.c
@@ -12,7 +12,10 @@
 size_t fpread(FILE *fp, size_t offset, size_t start, void *buffer, size_t size, size_t nitems)
 {
     if (fseek(fp, offset, start) != 0)
+    {
+        printf("ERROR");
         return 0;
+    }
     return fread(buffer, size, nitems, fp);
 }
 
@@ -49,19 +52,17 @@ SIFS_BIT *getBitmapPtr(const char *volumename, SIFS_VOLUME_HEADER header)
     SIFS_BIT *bitmap = malloc(header.nblocks * sizeof(int) + 1);
 
     //SKIP TO SPECIFIC BYTE COUNT THEN READ
-    fpread(fp, sizeof(header), SEEK_SET, bitmap, header.nblocks, header.nblocks);
+    //fpread(fp, sizeof(header), SEEK_SET, bitmap, header.nblocks, header.nblocks);
+    fseek(fp, sizeof(header), SEEK_SET);
+    fread(bitmap, 1, header.nblocks, fp);
 
+    fclose(fp);
+    //REVIEW WHY IS THERE A RUNTIME ERROR FOR fclose(fp)
+    //free(): invalid pointer
+    //Aborted(core dumped)
     printf("%s %d\n", bitmap, (int)(header.nblocks));
 
-    //fclose(fp);
     return bitmap;
-}
-
-int getDirBlockIdByName(const char *volumename, int currentBlockID, const char *dirname)
-{
-    // REVIEW
-    printf("CBID: %d, Dirname: %s\n", currentBlockID, dirname);
-    return 0;
 }
 
 char file_arr[24][32];           //file, or rather variable that stores directory path name
@@ -92,4 +93,44 @@ PATH getSplitPath(const char *pathname)
     }
     printf("\n");
     return path;
+}
+SIFS_DIRBLOCK getDirBlockById(const char *volumename, int currentBlockID)
+{
+    SIFS_VOLUME_HEADER header = getHeader(volumename);
+    SIFS_BIT *bitmap = getBitmapPtr(volumename, header); //REVIEW , bitmap never used
+    SIFS_DIRBLOCK *blockptr = malloc(header.blocksize + 1);
+
+    //OFFSET... header size, bitmap size, rootdir size, sizes of previous block
+    //roodir = sizes of previous block => (1+blockID-1)
+    int offset = sizeof(header) + header.nblocks + (currentBlockID) * (header.blocksize);
+    printf("OFFSET: %i bitmap:%c\n", offset, bitmap[currentBlockID]);
+
+    FILE *fp = fopen(volumename, "r+");
+    if (fp == NULL)
+    {
+        //READING ERROR
+        SIFS_errno = SIFS_ENOVOL;
+        //return;
+    }
+
+    //fpread(fp, offset, SEEK_SET, &block, sizeof(block), 1);
+    fseek(fp, offset, SEEK_SET);
+    fread(blockptr, header.blocksize, 1, fp);
+    fclose(fp);
+
+    printf("name: %s,modtime:%ld, nentries:%d, size of data: %ld  \n\n", blockptr->name, blockptr->modtime, blockptr->nentries, sizeof(SIFS_DIRBLOCK));
+    return *blockptr;
+}
+int getDirBlockIdByName(const char *volumename, int currentBlockID, const char *dirname)
+{
+    // REVIEW
+    printf("CBID: %d, Dirname: %s\n", currentBlockID, dirname);
+    SIFS_VOLUME_HEADER header = getHeader(volumename);
+    SIFS_BIT *bitmap = getBitmapPtr(volumename, header);
+    //OFFSET... header size, bitmap size, rootdir size, sizes of previous block
+    //roodir = sizes of previous block => (1+blockID-1)
+    int offset = sizeof(header) + header.nblocks + (currentBlockID) * (header.blocksize);
+    printf("OFFSET: %i bitmap:%c\n", offset, bitmap[1]);
+
+    return 0;
 }
