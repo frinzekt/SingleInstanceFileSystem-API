3266d7c0b6bbcc66f5622d3e59bfdd8b73a979c3
frinzekt <frinzekt@gmail.com>
2019-10-08 21:46:05 +0800

    Addition of Helper Function Header File and C File To Distributable

---
+++ sifs-files/library/helper.c
@@ -0,0 +1,13 @@
+/* CITS2002 Project 2 2019
+   Name(s):             Frinze Lapuz, Thye Shan Ng
+   Student number(s):   22711649, 22727425
+ */
+
+#include "sifs-internal.h"
+#include "helper.h"
+#include <stdio.h>
+
+void testPrint(void)
+{
+    printf("SUCCESSFUL");
+}
---
+++ sifs-files/library/helper.h
@@ -0,0 +1,18 @@
+/* CITS2002 Project 2 2019
+   Name(s):             Frinze Lapuz, Thye Shan Ng
+   Student number(s):   22711649, 22727425
+ */
+/* helper.h
+- Used for custom definition and function
+
+*/
+
+//Returns pointer that points to array of pointer pointing to strings
+//Can be done with char *words[number of words]
+extern char **getSplitPath(const char *pathname);
+extern int getDirBlockIdByName(int currentBlockID, const char *dirname);
+extern int getFileBlockIdByName(int currentBlockID, const char *filename);
+
+extern SIFS_DIRBLOCK getDirBlockById(int currentBlockID);
+extern SIFS_FILEBLOCK getFileBlockById(int currentBlockID);
+extern void testPrint(void);
