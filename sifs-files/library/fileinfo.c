f686dcc849ae1cb9d416a6b36702da8743d1820c
frinzekt <frinzekt@gmail.com>
2019-10-10 22:56:41 +0800

    Update fileinfo.c
    Initial Start

--- sifs-files/library/fileinfo.c
+++ sifs-files/library/fileinfo.c
@@ -3,9 +3,10 @@
    Student number(s):   22711649, 22727425
  */
 
+#include <stdio.h>
+#include <string.h>
 #include "sifs-internal.h"
 #include "helper.h"
-#include <string.h>
 
 // get information about a requested file
 int SIFS_fileinfo(const char *volumename, const char *pathname,
@@ -31,6 +32,12 @@ int SIFS_fileinfo(const char *volumename, const char *pathname,
     SIFS_FILEBLOCK block = getFileBlockById(fp, tailId);
     *length = block.length;
     *modtime = block.modtime;
+    printf("START HERE %li, %li\n", block.length, block.modtime);
+
+    printf("API OUTPUT STARTS HERE:\n");
+    printf("length: %ld ", *length);
+    printf("modtime: %ld", *modtime);
+    printf("API OUTPUT ENDS HERE:\n");
 
     SIFS_errno = SIFS_ENOTYET;
     return 1;
